95598952e275cc5072243b97c823f2cefa6b8cf6
aswin chandran <aswinchandranps@gmail.com>
2024-11-05 16:13:59 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent = React.createElement('div', {id:'parent'}, 
+    [React.createElement('div', {id:'child1'},
+        [React.createElement('h1', {id:'heading'}, 'I am h1 tag'),React.createElement('h2', {id:'heading2'}, 'I am h2 tag')] ),
+        React.createElement('div', {id:'child1'},
+            [React.createElement('h1', {id:'heading'}, 'I am h1 tag'),React.createElement('h2', {id:'heading2'}, 'I am h2 tag')] )]
+);
+
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(parent);
