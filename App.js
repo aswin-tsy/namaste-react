6d950afea3f9e116c66dc1b1a66153fad50a1de9
aswin chandran <aswinchandranps@gmail.com>
2024-11-12 11:47:22 +0000

    food app with api call

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import Header from "./src/componets/Header";
+import Body from "./src/componets/Body";
 
 
-const Header = () =>{
-    return(
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-        <div className="header">
-            <div className="logo-container">
-                <img src="https://designlogo6.wordpress.com/wp-content/uploads/2023/03/fast-food-logo-design-inspiration-7.jpg?w=500" className="logo"></img>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-const AppLayout = ()=>{
-    return (
-        <div className="app">
-            <Header/>
-
-        </div>
-    )
-}
-
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(<AppLayout/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
